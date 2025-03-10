192226275e17c5740093a9d48c1a0cbf33b9f429
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-10 22:42:27 +0100

    Aplicación probada desde App

--- src/main/java/dev/martabernardo/App.java
+++ src/main/java/dev/martabernardo/App.java
@@ -1,9 +1,19 @@
 package dev.martabernardo;
 
+import dev.martabernardo.controllers.SecuenceController;
+
 public final class App {
     private App() {
     }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SecuenceController secuenceController = SecuenceController.getInstance();
+        String adnSecuence = "ATCG";
+        String result = secuenceController.requestSecuence(adnSecuence);
+        System.out.println("DNA Secuence: " + adnSecuence);
+        System.out.println("RNA Secuence: " + result);
+        adnSecuence = "CCGA";
+        result = secuenceController.requestSecuence(adnSecuence);
+        System.out.println("DNA Secuence: " + adnSecuence);
+        System.out.println("RNA Secuence: " + result);
     }
 }
