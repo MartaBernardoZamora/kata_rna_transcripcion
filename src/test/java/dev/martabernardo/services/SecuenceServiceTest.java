61a01a254697d6b93c915194bd93248d7875d00d
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-10 20:56:29 +0100

    FAILED testSecuenceTranscribe from SecuenceServiceTest

--- src/test/java/dev/martabernardo/services/SecuenceServiceTest.java
+++ src/test/java/dev/martabernardo/services/SecuenceServiceTest.java
@@ -1,5 +1,6 @@
 package dev.martabernardo.services;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -8,13 +9,23 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SecuenceServiceTest {
+    SecuenceService secuenceService;
+    @BeforeEach
+    public void setUp() throws Exception {
+        secuenceService = SecuenceService.getInstance();        
+    }
     @Test
     public void testSecuenceService() {
-        SecuenceService secuenceService = SecuenceService.getInstance();
         SecuenceService instance2 = SecuenceService.getInstance();
-;
-
         assertThat(secuenceService, is(instanceOf(SecuenceService.class)));
         assertThat(secuenceService, equalTo(instance2));
+    }
+    @Test
+    public void testSecuenceTranscribe() {
+        String adnSecuence = "ATCG";
+        String expected = "UAGC";
+        String result = secuenceService.transcribe(adnSecuence);
+        assertThat(result, is(expected));
+
     }
 }
