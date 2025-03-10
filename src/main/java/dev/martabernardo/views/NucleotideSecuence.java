ed3daef99c979648dcb2e56a96dbabe0c5ac09f0
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-10 19:31:59 +0100

    STATUS OK testNucleotideSecuence from NucleotideSecuenceTest

---
+++ src/main/java/dev/martabernardo/views/NucleotideSecuence.java
@@ -0,0 +1,11 @@
+package dev.martabernardo.views;
+
+public class NucleotideSecuence {
+
+    protected String secuence;
+    
+    public NucleotideSecuence(String secuence) {
+        this.secuence = secuence;
+    }
+
+}
