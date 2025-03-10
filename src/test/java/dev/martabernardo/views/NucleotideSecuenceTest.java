8f46aba0fe414f57dd06c43a1336f1463a09b156
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-10 19:28:49 +0100

    FAILED testNucleotideSecuence from NucleotideSecuenceTest

---
+++ src/test/java/dev/martabernardo/views/NucleotideSecuenceTest.java
@@ -0,0 +1,18 @@
+package dev.martabernardo.views;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class NucleotideSecuenceTest {
+    @Test
+    public void testNucleotideSecuence() {
+        String secuence = "ATCG";
+        NucleotideSecuence nucleotideSecuence = new NucleotideSecuence(secuence);
+
+        assertThat(nucleotideSecuence, is(instanceOf(NucleotideSecuence.class)));
+
+    }
+}
