1a163c92fef087d090a2f7fd2e9fe39a0a2fd47e
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-10 20:16:51 +0100

    FAILED testNucleotideGetters from NucleotidesTest

--- src/test/java/dev/martabernardo/models/NucleotidesTest.java
+++ src/test/java/dev/martabernardo/models/NucleotidesTest.java
@@ -15,5 +15,17 @@ public class NucleotidesTest {
         assertThat(Nucleotides.GUANINE.name(), is("GUANINE"));
         assertThat(Nucleotides.THYMINE.name(), is("THYMINE"));
     }
+    @Test
+    void testNucleotideGetters() {
+        assertThat(Nucleotides.ADENINE.getDnaNucleotide(), is("A"));
+        assertThat(Nucleotides.CYTOSINE.getDnaNucleotide(), is("C"));
+        assertThat(Nucleotides.GUANINE.getDnaNucleotide(), is("G"));
+        assertThat(Nucleotides.THYMINE.getDnaNucleotide(), is("T"));
+
+        assertThat(Nucleotides.ADENINE.getRnaNucleotide(), is("U"));
+        assertThat(Nucleotides.CYTOSINE.getRnaNucleotide(), is("G"));
+        assertThat(Nucleotides.GUANINE.getRnaNucleotide(), is("C"));
+        assertThat(Nucleotides.THYMINE.getRnaNucleotide(), is("A"));
+    }
 
 }
